b51e8159e27d23f7c9d72403de9797f7f2676b4a
Vitor  Holtz <vitor.holtz@hartbgroup.com.br>
2021-11-14 19:00:28 -0300

    testo

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // EFEITO HEADER
-const body = document.body;
-let lastScroll = 0;
+// const body = document.body;
+// let lastScroll = 0;
 
-window.addEventListener('scroll', () =>{
+// window.addEventListener('scroll', () =>{
     
-    const currentScroll = window.pageYOffset
+//     const currentScroll = window.pageYOffset
 
-    if (currentScroll <= 0) {
-        body.classList.remove("scroll-up")
-    }
+//     if (currentScroll <= 0) {
+//         body.classList.remove("scroll-up")
+//     }
 
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-up")
-        body.classList.add("scroll-down")
-    }
-    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-down")
-        body.classList.add("scroll-up")
-    } 
+//     if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+//         body.classList.remove("scroll-up")
+//         body.classList.add("scroll-down")
+//     }
+//     if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+//         body.classList.remove("scroll-down")
+//         body.classList.add("scroll-up")
+//     } 
 
-    lastScroll = currentScroll
-})
+//     lastScroll = currentScroll
+// })
 
 // EFEITO TEXTO
 

5d7dd3637edc24af045ea3916afb01b95b3b4f8f
Vitor Holtz <vitor.holtz@hartbgroup.com.br>
2021-11-20 10:59:21 -0300

    tex-effect-test

--- script.js
+++ script.js
@@ -22,16 +22,30 @@ window.addEventListener('scroll', () =>{
     lastScroll = currentScroll
 });
 
-// EFEITO TEXTO
+// EFEITO TEXTO INTRO
 
-let ReadmoreManifestoBtn = document.querySelector('.read-more-button');
+let ReadmoreIntroBtn = document.querySelector('.read-more-button-intro');
+let IntroSection = document.querySelector('.cl-intro-text-wrapper');
+let IntroTextFade = document.querySelector('.intro-fade-black-bottom');
+
+ReadmoreIntroBtn.addEventListener ('click', () => {
+
+    IntroSection.classList.toggle('cl-manifesto-wrapper--opened');
+    ReadmoreIntroBtn.classList.toggle('read-more-button-intro--up');
+    IntroTextFade.classList.toggle('intro-fade-black-bottom--opened');
+    
+});
+
+// EFEITO TEXTO MANIFESTO
+
+let ReadmoreManifestoBtn = document.querySelector('.read-more-button-manifesto');
 let ManifestoSection = document.querySelector('.cl-manifesto-wrapper');
 let ManifestoTextFade = document.querySelector('.manifesto-fade-black-bottom');
 
 ReadmoreManifestoBtn.addEventListener ('click', () => {
 
     ManifestoSection.classList.toggle('cl-manifesto-wrapper--opened');
-    ReadmoreManifestoBtn.classList.toggle('read-more-button--up');
+    ReadmoreManifestoBtn.classList.toggle('read-more-button-manifesto--up');
     ManifestoTextFade.classList.toggle('manifesto-fade-black-bottom--opened');
     
 });
